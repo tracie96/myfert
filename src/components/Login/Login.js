daf747539f73c7093389dbe0e08d16f28ad93755
tracie96 <tracyamara07@gmail.com>
2025-05-22 16:43:19 +0100

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -195,7 +195,7 @@ function Login() {
               </Text>
               {/* <Text onClick={handleResendOtp} style={{ cursor: 'pointer', color: '#01ADF0' }}>Resend OTP?</Text> */}
               <Text className="sign-up-link">
-                Don't have an account? <NavLink to="/patientSignup">Sign Up</NavLink>
+                Don't have an account? <NavLink to="/clinicianRegister">Sign Up</NavLink>
               </Text>
             </div>
           </Form>
